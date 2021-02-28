9e065ff3a99be8155d94eaf9ad20c5f04968f042
itsmeharsh1997 <48449944+itsmeharsh1997@users.noreply.github.com>
2021-02-28 13:41:36 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+var btn = document.getElementById('btn-click');
+
+var wow = document.getElementById('sidebar')
+
+btn.addEventListener("click", function () {
+
+    if (wow.classList.contains('show')) {
+        wow.classList.remove('show')
+        wow.classList.add('hidden')
+
+    }
+    else {
+        wow.classList.remove('hidden')
+        wow.classList.add('show')
+
+    }
+
+
+})

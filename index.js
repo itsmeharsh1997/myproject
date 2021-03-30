ede2a0d78979517926ad7a4cffc1ce809801e6b0
itsmeharsh1997 <48449944+itsmeharsh1997@users.noreply.github.com>
2021-03-30 14:03:52 +0530

    Random Bike Number Plate Generator

--- index.js
+++ index.js
@@ -1,75 +1,15 @@
-function add() {
+function myFunction() {
+ var a = document.getElementById("demo").innerHTML = Math.random().toString().slice(2, 6);
 
+var b =document.getElementById("demo1").innerHTML = ("DL" + "3S" + a);
 
-    var x = parseInt(document.getElementById("varX").value);
-    var y = parseInt(document.getElementById("varY").value);
-
-
-    var result1 = x + y;
-
-
-    document.getElementById("varResult").value = result1;
-
-    return false;
 }
 
 
+function myFunction1() {
+  document.getElementById("demo").innerHTML = "";
+  document.getElementById("demo1").innerHTML = "";
 
-function subtract() {
-
-
-    var x = parseInt(document.getElementById("varX").value);
-    var y = parseInt(document.getElementById("varY").value);
-
-
-    var result2 = x - y;
-
-
-    document.getElementById("varResult").value = result2;
-
-    return false;
 }
 
 
-function multiply() {
-
-
-    var x = parseInt(document.getElementById("varX").value);
-    var y = parseInt(document.getElementById("varY").value);
-
-
-    var result3 = x * y;
-
-
-    document.getElementById("varResult").value = result3;
-
-    return false;
-}
-
-
-function divide() {
-
-
-    var x = parseInt(document.getElementById("varX").value);
-    var y = parseInt(document.getElementById("varY").value);
-
-
-    var result4 = x / y;
-
-
-    document.getElementById("varResult").value = result4;
-
-    return false;
-}
-
-
-function clear() {
-
-
-    var result4 = "";
-
-
-    document.getElementById("varResult").value = result4;
-
-    return false;
-}
